2ae65ed1e469fbfb62fa61d95092caa02014e183
Kevin Coxe <kevcoxe@gmail.com>
2020-04-26 08:25:16 -0400

    add stuff for gh-page

---
+++ app/precache-manifest.531d021c0d6b6bc92c4d275b25202352.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0643bb5a5e4c207973aed9fcd7f86c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d85a6d940f502fa9171b",
+    "url": "/static/js/2.c31c5593.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c31c5593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b74daf75fc6eded5af",
+    "url": "/static/js/main.3bbca49f.chunk.js"
+  },
+  {
+    "revision": "f483d1f7cd20d8e3322f",
+    "url": "/static/js/runtime-main.70302f69.js"
+  }
+]);
